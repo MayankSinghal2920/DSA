acb981ffe2565a592fe2498e8361c4ac71db8439
Mayank Singhal <mayanksinghal2920@gmail.com>
2026-01-19 17:48:15 +0530

    first commit

---
+++ src/basics/BasicMaths.java
@@ -0,0 +1,69 @@
+package basics;
+import java.util.*;
+
+public class BasicMaths {
+    static void main(String[] args) {
+
+//   #1. 3 number input and their product.
+        Scanner sc = new Scanner(System.in);
+
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//
+//        System.out.println( "Product of three numbers are = " +  a*b*c);
+
+
+
+//   #2. Average of input numbers
+
+//        int totNum = sc.nextInt();
+//        int sum = 0;
+//        for(int i=1; i<=totNum; i++){
+//            int num = sc.nextInt();
+//            sum += num;
+//        }
+//
+//        float average = sum/totNum;
+//        System.out.println(average);
+
+
+
+//    #3. Sum of first 10 numbers
+//        int sum = 0;
+//        for(int i=1; i<=10; i++){
+//            sum+= i;
+//
+//        }
+//        System.out.println(sum);
+
+//    #4. Sum of first 10 even numbers
+//        int sum = 0;
+//        for(int i=1; i<=10; i++){
+//            sum+= 2*i;
+//
+//        }
+//        System.out.println(sum);
+//
+//        int count = 1;
+//        int sum1 = 0;
+//        int num =2;
+//
+//        while(count<=10){
+//            sum1+= num;
+//            num = num+2;
+//            count = count+1;
+//        }
+//        System.out.println(sum1);
+
+//    #5. sum of even numbers between 20 and 40 both included
+//        int sum = 0;
+//        for(int i = 20; i<=40; i++){
+//            if(i%2 == 0){
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);
+    }
+
+}
