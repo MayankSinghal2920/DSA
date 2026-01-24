bdda682aa3163ee264f696416a7224847b5a37ca
MayankSInghal <mayanksinghal2920@gmail.com>
2026-01-24 16:15:24 +0530

    linear search

--- src/arrays/ArraysCC.java
+++ src/arrays/ArraysCC.java
@@ -2,16 +2,34 @@ package arrays;
 import java.util.*;
 
 public class ArraysCC {
+
+    public static int linearSearch(int arr[], int key){
+
+        for(int i=0; i<arr.length;i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
 
-        int marks[] = new int[10];
-        Scanner sc = new Scanner(System.in);
+//        int marks[] = new int[10];
+//        Scanner sc = new Scanner(System.in);
+//
+//        marks[0] = sc.nextInt();
+//        marks[1] = sc.nextInt();
+//
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
 
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
+        //Linear Search
 
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
+        int numbers[] = {12,32,345,53,54,78,96};
+        int key = 53;
+
+        System.out.println("Key is present at: "+linearSearch(numbers, key));
+
 
     }
 }
